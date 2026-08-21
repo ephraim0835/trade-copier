587b5ec383f20f1400c50560c1e563c10d3c8f19
Plaiz Design Studio <plaiz@plaizdesign.com>
2026-08-21 10:24:06 +0100

    fix: import EaAuthModule in AccountsModule

--- apps/api/src/modules/accounts/accounts.module.ts
+++ apps/api/src/modules/accounts/accounts.module.ts
@@ -3,8 +3,10 @@ import { AccountsController } from './controllers/accounts.controller';
 import { AccountsService } from './services/accounts.service';
 import { DatabaseModule } from '../../database/database.module';
 
+import { EaAuthModule } from '../../ea-auth/ea-auth.module';
+
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, EaAuthModule],
   controllers: [AccountsController],
   providers: [AccountsService],
   exports: [AccountsService],
